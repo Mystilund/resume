d8f5cc44b0dd6ee377526620d3d015c07e3edcd6
Mystilund <development.xplx@gmail.com>
2020-01-05 21:03:16 +0100

    Init the webapp

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react'
+import Navbar from './blocks/Navbar'
+
+const App = () => {
+  return (
+    <div className="app">
+      <Navbar />
+      <div className="page-content">
+        <p>things</p>
+      </div>
+    </div>
+  )
+}
+
+export default App
---
+++ src/blocks/Navbar.js
@@ -0,0 +1,17 @@
+import React from 'react'
+
+const Navbar = () => {
+  return (
+    <nav className="navbar">
+      <ul>
+        <li>Home</li>
+        <li>About</li>
+        <li>Works</li>
+        <li>Projects</li>
+        <li>Contact</li>
+      </ul>
+    </nav>
+  )
+}
+
+export default Navbar
