a8137db241e8216633c2179f7e8e6d3e8ec44645
Mystilund <development.xplx@gmail.com>
2020-01-08 20:03:25 +0100

    Add missing file

---
+++ src/components/SkillRank.js
@@ -0,0 +1,27 @@
+import React from 'react'
+import { number } from 'prop-types'
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faStar } from '@fortawesome/free-solid-svg-icons'
+
+const SkillRank = ({ rank }) => {
+  const getStars = (n) => {
+    return Array.from(n).map((_, index) => {
+      return <FontAwesomeIcon key={index} icon={faStar} />
+    })
+  }
+
+  return (
+    <div className="skill-rank">
+      {getStars(5)}
+      <div className="rank-stars">
+        {getStars(rank)}
+      </div>
+    </div>
+  )
+}
+
+SkillRank.propTypes = {
+  rank: number.isRequired
+}
+
+export default SkillRank
