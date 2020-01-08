fda54c03e7ccb9432d29745b948b37c07d0ff855
Mystilund <development.xplx@gmail.com>
2020-01-08 13:37:59 +0100

    Working on the skills/rank system

--- src/blocks/SkillBlock.js
+++ src/blocks/SkillBlock.js
@@ -1,7 +1,50 @@
-import React from 'react'
+import React, { useState } from 'react'
+import skills from '../assets/static-datas/skills.json'
+import Skill from '../components/Skill'
+import SkillRank from '../components/SkillRank'
 
 const SkillBlock = () => {
-  return <div>Skillblock</div>
+  const [currentSkillDisplayed, updateCurrentSkillDisplayed] = useState(null)
+
+  const openSkillTab = (index) => {
+    return () => {
+      updateCurrentSkillDisplayed(index)
+    }
+  }
+
+  const getRelatedTechnologies = () => {
+    if (currentSkillDisplayed === null || typeof skills[currentSkillDisplayed].technos === 'undefined') {
+      return null
+    }
+
+    return (
+      <div>
+        {
+          skills[currentSkillDisplayed].technos.map((skill, index) => {
+            return <p key={index}><a href={skill.link}>{skill.label}</a></p>
+          })
+        }
+      </div>
+    )
+  }
+
+  const generateSkills = () => {
+    return skills.map((skill, index) => {
+      return (
+        <Skill key={index} index={index} openSkillTab={openSkillTab}>
+          <span className="skill-name">{skill.label}</span>
+          {typeof skill.rank !== 'undefined' ? <SkillRank rank={skill.rank} /> : null }
+        </Skill>
+      )
+    })
+  }
+
+  return (
+    <div>
+      <div>{generateSkills()}</div>
+      <div>{getRelatedTechnologies()}</div>
+    </div>
+  )
 }
 
 export default SkillBlock
--- src/components/Skill.js
+++ src/components/Skill.js
@@ -1,9 +1,9 @@
 import React from 'react'
 import { number, func, oneOfType, arrayOf, node } from 'prop-types'
 
-const Skill = ({ index, openSkillTab, closeSkillTab, children }) => {
+const Skill = ({ index, openSkillTab, children }) => {
   return (
-    <div className="skill-line" onMouseEnter={openSkillTab(index)} onMouseLeave={closeSkillTab(index)}>
+    <div className="skill-line" onMouseEnter={openSkillTab(index)}>
       {children}
     </div>
   )
@@ -12,7 +12,6 @@ const Skill = ({ index, openSkillTab, closeSkillTab, children }) => {
 Skill.propTypes = {
   index: number.isRequired,
   openSkillTab: func.isRequired,
-  closeSkillTab: func.isRequired,
   children: oneOfType([arrayOf(node), node])
 }
 
