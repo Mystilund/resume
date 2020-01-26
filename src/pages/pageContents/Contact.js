c733f139b1ce532903dfcb94a39c26c563a53421
Mystilund <development.xplx@gmail.com>
2020-01-26 17:20:38 +0100

    Contact page finished

--- src/pages/pageContents/Contact.js
+++ src/pages/pageContents/Contact.js
@@ -1,7 +1,17 @@
 import React from 'react'
 
 const Contact = () => {
-  return <p>Contact</p>
+  return (
+    <div>
+      <h3>Am I looking for a new job ?</h3>
+      <p><strong>Yes</strong></p>
+      <p><br /></p>
+      <h3>How can you contact me ?</h3>
+      <p>By email <a href="mailto:recrutement.xplx@gmail.com">here</a></p>
+      <p><br /></p>
+      <p>I don't want to give my phone number because... well, I don't want it public and I don't want to be harrased</p>
+    </div>
+  )
 }
 
 export default Contact
